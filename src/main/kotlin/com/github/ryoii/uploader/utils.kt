8620a619d5ee683c528e39a3f78f6c48edd042a3
ryoii <ryoii@foxmail.com>
2020-07-06 23:00:23 +0800

    More ANSI for cursor

--- src/main/kotlin/com/github/ryoii/uploader/utils.kt
+++ src/main/kotlin/com/github/ryoii/uploader/utils.kt
@@ -7,3 +7,7 @@ fun InputStream.transform(output: OutputStream) = this.copyTo(output)
 
 fun upLine(n: Int) = if (n > 0) { print("\u001B[${n}F") } else Unit
 fun delLine() = println("\u001B[2F")
+fun saveCursor() = print("\u001B[s")
+fun recoverCursor() = print("\u001B[u")
+fun hideCursor() = print("\u001B[?25l")
+fun showCursor() = print("\u001B[?25h")

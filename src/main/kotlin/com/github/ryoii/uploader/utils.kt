d73df3cb6a979c7e1a5000b121848b74c0797a18
ryoii <ryoii@foxmail.com>
2020-07-12 10:38:47 +0800

    Fix ANSI code

--- src/main/kotlin/com/github/ryoii/uploader/utils.kt
+++ src/main/kotlin/com/github/ryoii/uploader/utils.kt
@@ -5,8 +5,8 @@ import java.io.OutputStream
 
 fun InputStream.transform(output: OutputStream) = this.copyTo(output)
 
-fun upLine(n: Int) = if (n > 0) { print("\u001B[${n}F") } else Unit
-fun delLine() = println("\u001B[2F")
+fun upLine(n: Int) = if (n > 0) { print("\u001B[${n}A") } else Unit
+fun delLine() = print("\u001B[2K")
 fun saveCursor() = print("\u001B[s")
 fun recoverCursor() = print("\u001B[u")
 fun hideCursor() = print("\u001B[?25l")

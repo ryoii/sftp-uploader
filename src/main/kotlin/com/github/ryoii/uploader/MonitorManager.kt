eae672254baca7dec8ac741f5ae6abe4a89bbb2f
ryoii <ryoii@foxmail.com>
2020-07-18 17:53:30 +0800

    Show left time

--- src/main/kotlin/com/github/ryoii/uploader/MonitorManager.kt
+++ src/main/kotlin/com/github/ryoii/uploader/MonitorManager.kt
@@ -19,7 +19,9 @@ object MonitorManager {
                 println("\r[${it.node.name}] ${100}% ${progressString(100)} 已完成")
             } else {
                 val percent = (100 * it.current / it.size).toInt()
-                println("\r[${it.node.name}] ${percent}% ${progressString(percent)} ${it.current / (System.currentTimeMillis() - it.start) } KB/s")
+                val speed = it.current / (System.currentTimeMillis() - it.start)
+                val left = ((it.size - it.current) shr 10) / speed
+                println("\r[${it.node.name}] ${percent}% ${progressString(percent)} ${speedStr(speed)} ${timeStr(left)}")
             }
 
             lineCount++
@@ -36,4 +38,17 @@ object MonitorManager {
             append("|")
         }
     }
+
+    private val speedArray = arrayOf("KB/s", "MB/s", "GB/s", "TB/s")
+    private fun speedStr(speed: Long): String {
+        var count = 0
+        var s = speed.toDouble()
+        while (s >= 1024.0) {
+            count++
+            s /= 1024.0
+        }
+        return String.format("%.2f %s", s, speedArray[count])
+    }
+
+    private fun timeStr(time: Long) = (time / 60).toString() + "m" + (time % 60).toShort() + "s"
 }
